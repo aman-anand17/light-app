cfd0f7e022ad2582a56588e8d9bd5fd62ccc3f14
aman-anand17 <legendneverretires@gmail.com>
2021-11-06 06:23:20 +0530

    first commit UI for map

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {
+  Routes,
+  Route
+} from 'react-router-dom';
+
+import Users from './user/pages/Users';
+import NewPlace from './places/pages/NewPlace';
+import UserPlaces from './places/pages/UserPlaces';
+import MainNavigation from './shared/components/Navigation/MainNavigation';
+
+const App = () => {
+  return (
+    <>
+      <MainNavigation />
+      <main>
+        <Routes>
+          <Route path="/" element={<Users/>}/>
+          <Route path="/:userId/places" element={<UserPlaces/>} />
+          <Route path="/places/new" element={<NewPlace />} />
+        </Routes>
+      </main>
+      </>
+  );
+};
+
+export default App;
